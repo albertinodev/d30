abb203c305f46c72cebc1b7f1d82609790c02b0a
Albertino Augusto <albertinoaugusto.it@gmail.com>
2022-05-07 21:32:39 +0200

    Create Heading1.tsx

---
+++ src/components/Heading1.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "react"
+
+
+interface Heading1Props {
+    children: ReactNode
+}
+
+const Heading1: React.FC<Heading1Props> = ({ children }) => {
+    return (
+        <h1 className="text-4xl font-semibold text-gray-800 dark:text-white">
+        Good afternoom, Charlie { children }
+        </h1>
+    )
+}
+
+export default Heading1;
