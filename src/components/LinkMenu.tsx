0e59bc80e39f99ba8003b6b215f7ee5f1656f0fa
Albertino Augusto <albertinoaugusto.it@gmail.com>
2022-05-07 21:04:12 +0200

    Update LinkMenu.tsx

--- src/components/LinkMenu.tsx
+++ src/components/LinkMenu.tsx
@@ -1,8 +1,4 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
 import Link from "next/link";
-import Seo from "components/Seo";
 
 const LinkMenu: React.FC = () => {
   return (
