28fa0343df1a4e73ae8c71e7ab977f32c871df54
Albertino Augusto <albertinoaugusto.it@gmail.com>
2022-05-07 21:08:59 +0200

    Update LinkMenu.tsx

--- src/components/LinkMenu.tsx
+++ src/components/LinkMenu.tsx
@@ -3,9 +3,10 @@ import { ReactNode } from "react";
 
 interface LinkProps {
     children: ReactNode
+    href: string
 }
 
-const LinkMenu: React.FC<LinkProps> = ({ children }) => {
+const LinkMenu: React.FC<LinkProps> = ({ children, href }) => {
   return (
     <Link href="/app">
       <a className="w-full text-gray-800 dark:text-white flex items-center pl-6 p-2 my-2 transition-colors duration-200 justify-start border-l-4 border-purple-500">
