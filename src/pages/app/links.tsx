2f7f5ada6e2d122c04dc89dc923c3176b851a625
Albertino Augusto <albertinoaugusto.it@gmail.com>
2022-05-07 21:38:19 +0200

    Update links.tsx

--- src/pages/app/links.tsx
+++ src/pages/app/links.tsx
@@ -1,13 +1,16 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
-import Seo from 'components/Seo'
+import type { NextPage } from "next";
+import Seo from "components/Seo";
+import Heading1 from "components/Heading1";
+import Heading2 from "components/Heading2";
 
 const Links: NextPage = () => {
   return (
-    <Seo title='Links'/>
-  )
-}
+    <>
+      <Seo title="Links" />
+      <Heading1>Seja bem vindo</Heading1>
+      <Heading2>Gerenciador de links</Heading2>
+    </>
+  );
+};
 
-export default Links
+export default Links;
