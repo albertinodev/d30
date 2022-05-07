311baf1a9c999c1d196762c8ccae17352ddd8720
Albertino Augusto <albertinoaugusto.it@gmail.com>
2022-05-07 21:05:42 +0200

    Update LinkMenu.tsx

--- src/components/LinkMenu.tsx
+++ src/components/LinkMenu.tsx
@@ -8,10 +8,8 @@ interface LinkProps {
 const LinkMenu: React.FC<LinkProps> = ({ children }) => {
   return (
     <Link href="/app">
-      <a
-        className="w-full text-gray-800 dark:text-white flex items-center pl-6 p-2 my-2 transition-colors duration-200 justify-start border-l-4 border-purple-500">
-
-
+      <a className="w-full text-gray-800 dark:text-white flex items-center pl-6 p-2 my-2 transition-colors duration-200 justify-start border-l-4 border-purple-500">
+        { children }
       </a>
     </Link>
   );
