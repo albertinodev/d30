01fcf12d112d0602ad1d11ca113f0fa28fda6a4f
Albertino Augusto <albertinoaugusto.it@gmail.com>
2022-05-07 21:00:28 +0200

    Create links.tsx

---
+++ src/pages/app/links.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import Seo from 'components/Seo'
+
+const Links: NextPage = () => {
+  return (
+    <Seo title='Links'/>
+  )
+}
+
+export default Links
