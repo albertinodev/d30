64c60905bc6d6bb5802fe1edb74a6f8772648bbe
Albertino Augusto <albertinoaugusto.it@gmail.com>
2022-05-07 21:42:17 +0200

    Update links.tsx

--- src/pages/app/links.tsx
+++ src/pages/app/links.tsx
@@ -9,6 +9,19 @@ const Links: NextPage = () => {
       <Seo title="Links" />
       <Heading1>Seja bem vindo</Heading1>
       <Heading2>Gerenciador de links</Heading2>
+      
+      <div className="flex items-center">
+          <button type="button" className="w-full border-l border-t border-b text-base font-medium rounded-l-md text-black bg-white hover:bg-gray-100 px-4 py-2">
+              Left
+          </button>
+          <button type="button" className="w-full border text-base font-medium text-black bg-white hover:bg-gray-100 px-4 py-2">
+              Center
+          </button>
+          <button type="button" className="w-full border-t border-b border-r text-base font-medium rounded-r-md text-black bg-white hover:bg-gray-100 px-4 py-2">
+              Right
+          </button>
+      </div>
+
     </>
   );
 };
