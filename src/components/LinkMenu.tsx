062a1005ed00efbe125c934e0dd9a97147c66451
Albertino Augusto <albertinoaugusto.it@gmail.com>
2022-05-07 21:12:31 +0200

    esfse

--- src/components/LinkMenu.tsx
+++ src/components/LinkMenu.tsx
@@ -7,6 +7,7 @@ interface LinkProps {
 }
 
 const LinkMenu: React.FC<LinkProps> = ({ children, href }) => {
+    //<a className="w-full text-gray-400 flex items-center pl-6 p-2 my-2 transition-colors duration-200 justify-start hover:text-gray-800 border-l-4 border-transparent" href="#">
   return (
     <Link href="/app">
       <a className="w-full text-gray-800 dark:text-white flex items-center pl-6 p-2 my-2 transition-colors duration-200 justify-start border-l-4 border-purple-500">
