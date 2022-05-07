09897e1d80c6b659ff864f39178c79fca0eed792
Albertino Augusto <albertinoaugusto.it@gmail.com>
2022-05-07 21:10:24 +0200

    Update LayoutApp.tsx

--- src/components/Layout/LayoutApp.tsx
+++ src/components/Layout/LayoutApp.tsx
@@ -1,3 +1,4 @@
+import LinkMenu from "components/LinkMenu";
 import Link from "next/link";
 import { ReactNode } from "react";
 
@@ -18,8 +19,7 @@ const LayoutApp: React.FC<LayoutProps> = ({ children }) => {
                       </div>
                       <nav className="mt-6">
                           <div>
-                              <Link href='/app'>
-                                <a className="w-full text-gray-800 dark:text-white flex items-center pl-6 p-2 my-2 transition-colors duration-200 justify-start border-l-4 border-purple-500" href="#">
+                              <LinkMenu href='/app'>
                                     <span className="text-left">
                                         <svg width="20" height="20" fill="currentColor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M1472 992v480q0 26-19 45t-45 19h-384v-384h-256v384h-384q-26 0-45-19t-19-45v-480q0-1 .5-3t.5-3l575-474 575 474q1 2 1 6zm223-69l-62 74q-8 9-21 11h-3q-13 0-21-7l-692-577-692 577q-12 8-24 7-13-2-21-11l-62-74q-8-10-7-23.5t11-21.5l719-599q32-26 76-26t76 26l244 204v-195q0-14 9-23t23-9h192q14 0 23 9t9 23v408l219 182q10 8 11 21.5t-7 23.5z">
@@ -29,8 +29,7 @@ const LayoutApp: React.FC<LayoutProps> = ({ children }) => {
                                     <span className="mx-2 text-sm font-normal">
                                         Home
                                     </span>
-                                </a>
-                              </Link>
+                              </LinkMenu>
                               <Link href='/app/kjehfsh'>
                                 <a className="w-full text-gray-400 flex items-center pl-6 p-2 my-2 transition-colors duration-200 justify-start hover:text-gray-800 border-l-4 border-transparent" href="#">
                                     <span className="text-left">
