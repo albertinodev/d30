f7600fa765cc79f71bf51fbc72efdbda66174acf
Albertino Augusto <albertinoaugusto.it@gmail.com>
2022-05-07 21:44:38 +0200

    Update links.tsx

--- src/pages/app/links.tsx
+++ src/pages/app/links.tsx
@@ -6,22 +6,23 @@ import Heading2 from "components/Heading2";
 const Links: NextPage = () => {
   return (
     <>
-      <Seo title="Links" />
-      <Heading1>Seja bem vindo</Heading1>
-      <Heading2>Gerenciador de links</Heading2>
-      
-      <div className="flex items-center">
-          <button type="button" className="w-full border-l border-t border-b text-base font-medium rounded-l-md text-black bg-white hover:bg-gray-100 px-4 py-2">
-              Left
-          </button>
-          <button type="button" className="w-full border text-base font-medium text-black bg-white hover:bg-gray-100 px-4 py-2">
-              Center
-          </button>
-          <button type="button" className="w-full border-t border-b border-r text-base font-medium rounded-r-md text-black bg-white hover:bg-gray-100 px-4 py-2">
-              Right
-          </button>
+      <div className="">
+        <Seo title="Links" />
+        <Heading1>Seja bem vindo</Heading1>
+        <Heading2>Gerenciador de links</Heading2>
+        
+        <div className="flex items-center">
+            <button type="button" className="w-full border-l border-t border-b text-base font-medium rounded-l-md text-black bg-white hover:bg-gray-100 px-4 py-2">
+                Criar Link
+            </button>
+            <button type="button" className="w-full border text-base font-medium text-black bg-white hover:bg-gray-100 px-4 py-2">
+                Criar Grupo
+            </button>
+            <button type="button" className="w-full border-t border-b border-r text-base font-medium rounded-r-md text-black bg-white hover:bg-gray-100 px-4 py-2">
+                Right
+            </button>
+        </div>
       </div>
-
     </>
   );
 };
