c2c3d1d9d0c94d4773fec91f95d72da149ae09a8
Albertino Augusto <albertinoaugusto.it@gmail.com>
2022-05-07 21:02:33 +0200

    Create LinkMenu.tsx

---
+++ src/components/LinkMenu.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import Seo from 'components/Seo'
+
+const LinkMenu: React.FC = () => {
+  return (
+    <Seo title='Siginin'/>
+  )
+}
+
+export default LinkMenu
