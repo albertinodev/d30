6efecdbd9b2217ebb3f53a12b7a2d96c673bd57c
Albertino Augusto <albertinoaugusto.it@gmail.com>
2022-05-07 21:05:27 +0200

    Update LinkMenu.tsx

--- src/components/LinkMenu.tsx
+++ src/components/LinkMenu.tsx
@@ -1,12 +1,18 @@
 import Link from "next/link";
+import { ReactNode } from "react";
 
-const LinkMenu: React.FC = () => {
+interface LinkProps {
+    children: ReactNode
+}
+
+const LinkMenu: React.FC<LinkProps> = ({ children }) => {
   return (
     <Link href="/app">
       <a
-        className="w-full text-gray-800 dark:text-white flex items-center pl-6 p-2 my-2 transition-colors duration-200 justify-start border-l-4 border-purple-500"
-        href="#"
-      ></a>
+        className="w-full text-gray-800 dark:text-white flex items-center pl-6 p-2 my-2 transition-colors duration-200 justify-start border-l-4 border-purple-500">
+
+
+      </a>
     </Link>
   );
 };
