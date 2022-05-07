4d0f6f68a784046cae54235e46c9b88c9782b810
Albertino Augusto <albertinoaugusto.it@gmail.com>
2022-05-07 21:45:43 +0200

    Update links.tsx

--- src/pages/app/links.tsx
+++ src/pages/app/links.tsx
@@ -6,11 +6,12 @@ import Heading2 from "components/Heading2";
 const Links: NextPage = () => {
   return (
     <>
-      <div className="">
-        <Seo title="Links" />
-        <Heading1>Seja bem vindo</Heading1>
-        <Heading2>Gerenciador de links</Heading2>
-        
+      <Seo title="Links" />
+      <div className="grid grid-cols-2">
+        <div>
+          <Heading1>Seja bem vindo</Heading1>
+          <Heading2>Gerenciador de links</Heading2>
+        </div>
         <div className="flex items-center">
             <button type="button" className="w-full border-l border-t border-b text-base font-medium rounded-l-md text-black bg-white hover:bg-gray-100 px-4 py-2">
                 Criar Link
